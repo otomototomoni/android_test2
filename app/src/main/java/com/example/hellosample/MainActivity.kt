c11db9b00c46c4aeb7bc47cfc26759c267ed032a
otomototomoni <tegurechafu150415@icloud.com>
2024-06-28 13:32:30 +0900

    MainActivity.kt override fun onCreateにbtClick,listener,btClickにリスナを設定を追加

--- app/src/main/java/com/example/hellosample/MainActivity.kt
+++ app/src/main/java/com/example/hellosample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.hellosample
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.ComponentActivity
@@ -10,6 +11,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //表示ボタンであるButtonオブジェクトを取得
+        val btClick = findViewById<Button>(R.id.btClick)
+        //リスナクラスのインスタンスを生成
+        val listener = HelloListener()
+        //表示ボタンにリスナを設定
+        btClick.setOnClickListener(listener)
     }
 
     //ボタンをクリックしたときのリスナクラス
