0a390adbb8a7e56adc9dbc085d00525bf42acbdb
otomototomoni <tegurechafu150415@icloud.com>
2024-06-28 13:25:29 +0900

    MainActivity.ktにリスナクラス（HelloListener）の追加

--- app/src/main/java/com/example/hellosample/MainActivity.kt
+++ app/src/main/java/com/example/hellosample/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.hellosample
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    //ボタンをクリックしたときのリスナクラス
+    private inner class HelloListener : View.OnClickListener {
+        override fun onClick(view: View) {
+            //名前入力欄であるEditTextオブジェクトを取得
+            val input = findViewById<EditText>(R.id.etName)
+            //メッセージを表示するTextViewオブジェクトを取得
+            val output = findViewById<TextView>(R.id.btClick)
+            //入力された名前文字列を取得
+            val inputStr = input.text.toString()
+            //メッセージを表示
+            output.text = inputStr + "さん、こんにちは！"
+        }
     }
 }
